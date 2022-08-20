315a69f5252594fe3e7de6ce72d1a154c0280f36
David <davidstepanov@yahoo.com>
2022-08-20 18:06:00 -0400

    Attempt number 4 to update the JS file

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2= document.createElement("h2")
+const h2 = document.createElement("h2")
 h2.textContent = "Here's a photo of a capybara with a cat"
 document.querySelector("body").appendChild(h2)
